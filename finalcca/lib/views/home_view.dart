60b4d01175cbdba9b3a8f660146fe0391e6e460e
Bulatuly Nurgissa <nura@MacBook-Air-Bulatuly.local>
2021-11-16 17:09:07 +0600

    2.0

--- finalcca/lib/views/home_view.dart
+++ finalcca/lib/views/home_view.dart
@@ -3,14 +3,30 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
-class HomewView extends StatelessWidget {
+import 'package:url_launcher/url_launcher.dart';
 
+class HomewView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var locationModel = Provider.of<LocationModel>(context);
+    _launchURL() async {
+      var url = 'http://20.83.163.49:8080/gps-tracking-writer/add-tracking?loc=${locationModel.latitude},%20${locationModel.longitude}';
+      if (await canLaunch(url)) {
+        await launch(url);
+      } else {
+        throw 'Could not launch $url';
+      }
+    }
     return Scaffold(
       appBar: AppBar(
-        title:  Text('${locationModel.latitude}, ${locationModel.longitude}')
+        title:  Text('${locationModel.latitude}, ${locationModel.longitude}'),
+        leading: GestureDetector(
+          onTap: _launchURL,
+          child: Icon(
+            Icons.search,
+            size: 26.0,
+          ),
+        ),
       ),
       body: FlutterMap(
         options: MapOptions(
